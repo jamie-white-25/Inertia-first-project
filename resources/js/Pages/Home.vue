fe506699a1dfd4659ee2ad09f6313d4fe26bef49
jamie-white-25 <jw4gcc127@gmail.com>
2022-03-21 23:38:32 +0000

    added taillwind
    Added tailwind and first compoent.

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-gray-300"> 
+    <h1>hello world, number of clicks {{ counter }}</h1>
+    <h2>{{ props.user }}</h2>
+    <button @click="counter++">add</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  user: String,
+});
+
+const counter = ref(0);
+</script>
+
+<style>
+</style>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return inertia('home');
+    return inertia('Home', [
+        'user' => "Joe doh"
+    ]);
 });
